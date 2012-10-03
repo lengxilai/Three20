30763e6b58d91cd8388f9f0e51f2f73a34aa748c
lipeng <lp508@163.com>
2012-10-03 14:40:29 +0800

    first commit

---
+++ Three20/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Three20
+//
+//  Created by 鹏 李 on 12-10-3.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "IGAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([IGAppDelegate class]));
+    }
+}
